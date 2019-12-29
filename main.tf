ccaa51c9100df5ac170965b1188c258b4590d232
Galina Yudina <galina.yudina@gmail.com>
2019-12-30 10:32:31 +1100

    Refactor to terraform 0.12, add labels to vm

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
 # Create an instance with OS Login configured to use as a bastion host.
 
 resource "google_compute_instance" "bastion_host" {
-  name         = "${var.instance_name}"
-  machine_type = "${var.machine_type}"
-  zone         = "${var.zone}"
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
 
-  tags = "${var.tags}"
+  tags = var.tags
+  labels = var.labels
 
   boot_disk {
     initialize_params {
-      image = "${var.source_image}"
+      image = var.source_image
     }
   }
 
   network_interface {
-    subnetwork = "${var.subnetwork}"
+    subnetwork = var.subnetwork
 
     // Provide an empty access_config block to receive an ephemeral IP
     access_config {}
